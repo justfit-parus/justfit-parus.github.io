07bfe9adf3f60ef43a920090e702e0629914ab71
justfit-parus <polyakov@ra-parus.ru>
2016-10-12 18:29:25 +0400

    123123
    123123

--- js/common.js
+++ js/common.js
@@ -1,85 +1,138 @@
 $(document).ready(function() {
 
-    var tools = $(".tools-slider");
-    var secret = $(".secret-slider");
-    var checkWidth = $(window).width();
+    $(window).scroll(function() {
+      var st = $(this).scrollTop();
+      $('.present-img').css ({
+        'transform' : "translate(0%, -" + st/50 + "%"
+      });
+    });
 
-    tools.owlCarousel({
+    $('#blogapp-carousel').owlCarousel({
         loop: true,
-        responsive: true,
-        singleItem: true,
-        itemClass: "tools-item"
+        margin: 0,
+        items: 1,
+        dots: false,
+        autoplay: true
     });
 
-    $(".tools-next, .tools-m-next").click(function() {
-        tools.trigger('owl.next');
+    $(".blogapp-next").click(function() {
+        $('#blogapp-carousel').trigger('next.owl.carousel');
     });
-    $(".tools-prev, .tools-m-prev").click(function() {
-        tools.trigger('owl.prev');
+    $(".blogapp-prev").click(function() {
+        $('#blogapp-carousel').trigger('prev.owl.carousel');
     });
 
-    function owlSecret() {
-        if (checkWidth > 801) {
-            secret.data('owlCarousel').destroy();
-            secret.removeClass('owl-carousel');
-        } else if (checkWidth < 800) {
-            secret.owlCarousel({
-                loop: true,
-                responsive: true,
-                singleItem: true,
-                itemClass: "tools-item",
-            });
-        }
-        $(document).ready(owlSecret);
-        $(window).resize(owlSecret);
+    $('#nativator-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        autoplay: true
+    });
 
-        $(".secret-next").click(function() {
-            secret.trigger('owl.next');
+    $(".nativator-next").click(function() {
+        $('#nativator-carousel').trigger('next.owl.carousel');
+    });
+    $(".nativator-prev").click(function() {
+        $('#nativator-carousel').trigger('prev.owl.carousel');
+    });
+
+    $('#car-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000
+    });
+
+    $(".app-next").click(function() {
+        $('#car-carousel').trigger('next.owl.carousel');
+    });
+    $(".app-prev").click(function() {
+        $('#car-carousel').trigger('prev.owl.carousel');
+    });
+
+    $('#phone1').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        lazyLoad: true,
+        autoplay: true,
+        autoplayTimeout: 3000
+    });
+    setTimeout(function() {
+        $('#phone2').owlCarousel({
+            loop: true,
+            margin: 0,
+            items: 1,
+            dots: false,
+            lazyLoad: true,
+            autoplay: true,
+            autoplayTimeout: 3000
         });
-        $(".secret-prev").click(function() {
-            secret.trigger('owl.prev');
+    }, 200)
+    setTimeout(function() {
+        $('#phone3').owlCarousel({
+            loop: true,
+            margin: 0,
+            items: 1,
+            dots: false,
+            lazyLoad: true,
+            autoplay: true,
+            autoplayTimeout: 3000
         });
-    }
+    }, 600)
 
-    //Popups
-    $('.call').click(function(event) { // лoвим клик
-        event.preventDefault(); // выключaем стaндaртную рoль элементa
-        $('.popup, .overlay').css('display', 'block'); // убирaем у мoдaльнoгo oкнa display: none;
-        $('.popup, .overlay').animate({
-            opacity: 1
-        }, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+    $(".mag-app-next").click(function() {
+        $('#phone1').trigger('next.owl.carousel');
+        setTimeout(function() {
+            $('#phone2').trigger('next.owl.carousel');
+        }, 200)
+        setTimeout(function() {
+            $('#phone3').trigger('next.owl.carousel');
+        }, 400)
     });
-    //Popup buisness
-    $('.call-buisness').click(function(event) { // лoвим клик
-        event.preventDefault(); // выключaем стaндaртную рoль элементa
-        $('.popup-buisness, .overlay').css('display', 'block'); // убирaем у мoдaльнoгo oкнa display: none;
-        $('.popup-buisness, .overlay').animate({
-            opacity: 1
-        }, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
-    });
-
-    //popup close
-    $('.close-button, .overlay').click(function() { // лoвим клик пo крестику или пoдлoжке
-        $('.popup, .popup-buisness, .overlay')
-            .animate({
-                    opacity: 0
-                }, 200, // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
-                function() { // пoсле aнимaции
-                    $(this).css('display', 'none'); // делaем ему display: none;
-                }
-            );
+    $(".mag-app-prev").click(function() {
+        $('#phone1').trigger('prev.owl.carousel');
+        setTimeout(function() {
+            $('#phone2').trigger('prev.owl.carousel');
+        }, 200)
+        setTimeout(function() {
+            $('#phone3').trigger('prev.owl.carousel');
+        }, 400)
     });
 
 
-
-    //mobile menu
-    $('.m-header').click(function(event) { // лoвим клик
-        event.preventDefault(); // выключaем стaндaртную рoль элементa
-        $('.humburger').toggleClass('open');
-        $('.list-mobile').slideToggle('fast');
+    // popup
+    $('.lang-on').click(function(event) {
+        event.preventDefault();
+        $('.popup-lang').css('display', 'flex');
+        $('.wrapper').css('filter', 'blur(30px)');
+        $('.main-wrapper').css({
+            'position': 'fixed',
+            'width': '100%',
+            'margin': '0 auto'
+        });
     });
 
-    //Chrome Smooth Scroll
+    // popup close
+    $('.popup-lang, .lang li').click(function() {
+        $('.popup-lang').css('display', 'none');
+        $('.wrapper').css('filter', 'none');
+        $('.main-wrapper').css({
+            'position': 'relative',
+            'margin': '0'
+        });
+    });
+
+    // close lang panel
+    $('.lang-off').click(function() {
+        $('.lang').css('display', 'none');
+    })
+
+    // chrome Smooth Scroll
     try {
         $.browserSelector();
         if ($("html").hasClass("chrome")) {
@@ -92,5 +145,4 @@ $(document).ready(function() {
     $("img, a").on("dragstart", function(event) {
         event.preventDefault();
     });
-
 });
